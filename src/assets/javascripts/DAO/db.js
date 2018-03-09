da3cfb4a93469d395228cec2562cfede021f1393
f.wattier <skflo60@hotmail.com>
2018-03-09 20:54:29 +0100

    inital commit 2

--- src/assets/javascripts/DAO/db.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-const db = new Dexie('feelgoodDB');
-db.version(1).stores({ clocks: '++id, datetime' });
-
-export default db;
--- src/assets/javascripts/actions/clock_actions.js
+++
@@ -1,8 +0,0 @@
-import Reflux from "reflux";
-
-const ClockActions = Reflux.createActions([
-	"loadClocks",
- 	"clockAdd"
-]);
-
-export default ClockActions;
